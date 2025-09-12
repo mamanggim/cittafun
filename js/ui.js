b4b673c33627be0feb79af38ffcc39a4e05e29b5
mamanggim <mamanggim@gmail.com>
2025-09-13 06:17:01 +0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,7 @@
+// Toggle navbar
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
 
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
