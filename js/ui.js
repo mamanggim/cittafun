5f5be5e6c0e6e3fa19ea8e71fd2418a703510234
mamanggim <mamanggim@gmail.com>
2025-09-13 07:09:37 +0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,16 @@
 // Toggle navbar
 const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
+const overlay = document.getElementById("overlay");
 
 menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
   navLinks.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  menuToggle.classList.remove("active");
+  navLinks.classList.remove("active");
+  overlay.classList.remove("active");
 });
